cc63320438f461cdbc453f8c4587aa5269154e61
nahweijie1 <weijienah1@gmail.com>
2024-06-22 12:26:31 +0800

    Quick clean up of functions created before and included docstrings

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
-path_to_file = "books/frankenstein.txt"
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    print(f"{num_words} words found in the document")
 
-def count_words(text):
-    '''Takes in a string `text` and returns the number of words in `text` counted as an `int`'''
+
+def get_num_words(text):
+    '''Takes in a string `text` and returns the number of words within `text` as an `int`'''
     words = text.split()
-    print(f' Given text has {len(words)} words.')
     return len(words)
 
-def main():
-    with open(path_to_file) as f:
-        file_contents = f.read()
-        
-    count_words(file_contents)
-    
-main()
 
-    
-    
+def get_book_text(path):
+    '''Takes in a string `path` to a file, calls `open()` on the file and returns the content of the file as a `string` using `read()`'''
+    with open(path) as f:
+        return f.read()
+
+
+main()
