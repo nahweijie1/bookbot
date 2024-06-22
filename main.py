ff4385a9275b9ca0678f94204460ea540cbc6fa0
nahweijie1 <weijienah1@gmail.com>
2024-06-22 14:10:38 +0800

    Revision done on docstrings, cleaned up methods with reference to solution

--- main.py
+++ main.py
@@ -1,28 +1,12 @@
-def main():
-    book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
-    num_words = get_num_words(text)
-    num_char_dict = get_character_counts(text)
-    print(f"{num_words} words found in the document")
-    print(f'{len(num_char_dict)} distinct characters found in the document: ')
-    for k, v in num_char_dict.items():
-        print(f"{k}: {v}")
-
-
 def get_num_words(text):
-    '''Takes in a string `text` and returns the number of words within `text` as an `int`'''
     words = text.split()
     return len(words)
 
-
 def get_book_text(path):
-    '''Takes in a string `path` to a file, calls `open()` on the file and returns the content of the file as a `string` using `read()`'''
     with open(path) as f:
         return f.read()
 
 def get_character_counts(text):
-    '''Takes in a string `text` and returns the count of each character as a `dict` with a key: value pair of `'character': count` of type `string: int`
-        * Note: This method does not take into account case sensitivity as `.lower()` is called on text before performing character count. '''
     character_dict = {}
     for char in text.lower():
         if char not in character_dict:
@@ -30,5 +14,42 @@ def get_character_counts(text):
         else:
             character_dict[char] += 1
     return character_dict
+
+def chars_dict_to_sorted_list(dict):
+    sorted_list = []
+    for k, v in dict.items():
+        sorted_list.append({'char': k, 'count': v})
+    return sorted_list
+
+def sort_on(dict):
+    return dict['count']
+
+def generate_report_header(text):
+        print(f"--- Begin report of {text} ---")
+    
+def generate_report_footer():
+        print(f"--- End report ---")
+    
+def generate_report(book_path):
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    num_char_dict = get_character_counts(text)
+    characters = chars_dict_to_sorted_list(num_char_dict)
+    characters.sort(reverse = True, key=sort_on)
+    
+    generate_report_header(book_path)
+    print(f"{num_words} words found in {book_path}")
+    print("")
+    for item in characters:
+        if not item['char'].isalpha():
+            continue
+        print(f" The '{item['char']}' character was found {item['count']} times")
+    generate_report_footer()
+
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    key_list = ['char','count']
+    generate_report(book_path)
 
 main()
