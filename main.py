18ec8200b606e732e9291d7a61d349c9ca28a36a
nahweijie1 <weijienah1@gmail.com>
2024-06-22 12:41:39 +0800

    Added get_character_counts and included docstring for its method

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
+    num_char_dict = get_character_counts(text)
     print(f"{num_words} words found in the document")
+    print(f'{len(num_char_dict)} distinct characters found in the document: ')
+    for k, v in num_char_dict.items():
+        print(f"{k}: {v} in the document")
 
 
 def get_num_words(text):
@@ -16,5 +20,15 @@ def get_book_text(path):
     with open(path) as f:
         return f.read()
 
+def get_character_counts(text):
+    '''Takes in a string `text` and returns the count of each character as a `dict` with a key: value pair of `'character': count` of type `string: int`
+        * Note: This method does not take into account case sensitivity as `.lower()` is called on text before performing character count. '''
+    character_dict = {}
+    for char in text.lower():
+        if char not in character_dict:
+            character_dict.update({char: 0})
+        else:
+            character_dict[char] += 1
+    return character_dict
 
 main()
